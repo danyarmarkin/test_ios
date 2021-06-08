aee3229480df81d94734550c9f28aea46e93398d
Данила Ярмаркин <danyarmarkin@MacBook-Air.local>
2021-06-08 16:46:34 +0300

    Initial commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Данила Ярмаркин on 08.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello Kanistra")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/test1App.swift
@@ -0,0 +1,17 @@
+//
+//  test1App.swift
+//  Shared
+//
+//  Created by Данила Ярмаркин on 08.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
